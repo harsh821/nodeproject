fe9fad354d4efcf6e3b11f0fada527d4a229a147
Harsh <rathodh13@gmail.com>
2020-08-13 18:59:41 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const path=require('path')
+
+const app = express()
+const port = 3000
+
+
+app.use(express.urlencoded({ extended: false }));
+
+//setting the view
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+})
+
+app.use(express.static(path.join(__dirname, '/views')));
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
